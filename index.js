6432f1640fa4394ffcefeb77e0b5a4bfdb13efb0
alechp <me@alechp.com>
2017-09-25 17:48:09 -0700

    Overhauled index. For some reason it doesn't like my dynamic importing

--- index.js
+++ index.js
@@ -1,107 +1,47 @@
-// const {
-//   QueryBuilder,
-//   builder,
-//   QueryBatcher,
-//   batcher
-// } = require("../graphql-query-factory/dist/index.js");
+const mock = require("./_mock.js");
+let library = {
+  local: "../graphql-query-factory/dist/index.js",
+  remote: "graphql-query-factory"
+}
 
-const {
-  QueryBuilder,
-  builder,
-  QueryBatcher,
-  batcher
-} = require("graphql-query-factory");
-
+const graphqlQueryFactory = require(library.local);
+// const { builder, batcher, factory } = graphqlQueryFactory;
+const builder = graphqlQueryFactory.builder;
+const batcher = graphqlQueryFactory.batcher;
+const factory = graphqlQueryFactory.factory;
 const chalk = require("chalk");
 const log = console.log;
 
-const mutationTemplate = `mutation {
-  createContent(
-    markup: $markup
-    raw: $raw
-  ) {
-    markup
-    raw
-  }
-}`;
+log(
+  `\n------------------------------\n
+    ${chalk.green("Builder")}
+  \n------------------------------\n}`
+);
+let queries = builder(mock.template, mock.variables);
+log(queries);
+////////////////////////////////////////////////////////
+log(
+  `\n------------------------------\n
+    ${chalk.green("Batcher (single)")}
+  \n------------------------------\n}`
+);
+let responseSolo = await batcher.request(mock.singleQuery);
+log(responseSolo);
+////////////////////////////////////////////////////////
+log(
+  `\n------------------------------\n
+    ${chalk.green("Batcher (multiple)")}
+  \n------------------------------\n}`
+);
 
-const mutationVariables = [
-  {
-    markup: "markup1",
-    raw: "raw1"
-  },
-  {
-    markup: "markup2",
-    raw: "raw2"
-  },
-  {
-    markup: "markup3",
-    raw: "raw3"
-  }
-];
+let responseMultiple = await batcher.batch(mock.batchQuery);
+log(responseMultiple);
 
-function manMutationBuild() {
-  const qbuilder = new QueryBuilder();
-  qbuilder.setQuery(mutationTemplate);
-  qbuilder.setVariables(mutationVariables);
-  let queries = qbuilder.buildQueries();
-  return queries;
+log(
+  `\n------------------------------\n
+    ${chalk.green("Factory")}
+  \n------------------------------\n}`
+);
+let responseFactory = awawit factory(mock.template, mockvariables);
+log(responseFactory);
 }
-function autoMutationBuild() {
-  let queries = builder(mutationTemplate, mutationVariables);
-  return queries;
-}
-
-//-----------------------------------
-const manMutations = manMutationBuild();
-const autoMutations = autoMutationBuild();
-
-function manMutationExecES6() {
-  //concurrency defaults to 4, so no need to specify if you want 4 concurrent requests
-  const qbatcher = new QueryBatcher(manMutations, 4);
-  qbatcher
-    .queryBatchExecute()
-    .then(queries => log(`Queries: ${queries}`))
-    .catch(err => log(`manMutationExecES6: ${chalk.red(err)}`));
-}
-async function manMutationExecES7() {
-  //maybe you want to use a global query batcher, in which case you can programmatically set mutations & concurrent connections
-  const qbatcher = new QueryBatcher();
-  qbatcher.setQueries(manMutations);
-  qbatcher.setConcurrent(4);
-  try {
-    let queries = await qbatcher.queryBatchExecute();
-  } catch (err) {
-    log(`manMutationExecES7: ${chalk.red(err)}`);
-  }
-}
-function autoMutationExecES6() {
-  batcher(manMutations, 4)
-    .then(res => log(`autoMutationExecES6: ${res}`))
-    .catch(err => log(`autoMutationExecES6: ${err}`));
-}
-async function autoMutationExecES7() {
-  try {
-    log(await batcher(manMutations, 4));
-  } catch (err) {
-    log(`autoMutationExecES7: ${err}`);
-  }
-}
-////////////////////////////////////////////////////
-// BUILDER
-////////////////////////////////////////////////////
-log(`${chalk.yellow("\n manMutationBuild: \n")}`);
-manMutationBuild();
-log(`${chalk.yellow("\n autoMutationBuild: \n")}`);
-autoMutationBuild();
-////////////////////////////////////////////////////
-//BATCHER
-////////////////////////////////////////////////////
-// log(`${chalk.yellow("\n manMutationExecES6: \n")}`);
-manMutationExecES6();
-// log(`${chalk.yellow("\n manMutationExecES7: \n")}`);
-manMutationExecES7();
-// log(`${chalk.yellow("\n autoMutationExecES6: \n")}`);
-autoMutationExecES6();
-// log(`${chalk.yellow("\n autoMutationExecES7: \n")}`);
-autoMutationExecES7();
